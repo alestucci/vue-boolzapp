24f373b2723972f0a4dcad082546dd8ed80a110a
alestucci <alessandrotucci@gmail.com>
2022-03-18 00:24:51 +0100

    Milestone 5 completed

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ const app = new Vue({
 		},
 		activeIndex: 0,
 		searchString: "",
+		msgMenuActive: null,
 		chatArray: [
 			{
 				name: "Michele",
@@ -14,23 +15,29 @@ const app = new Vue({
 				newMessage: "",
 				messages: [
 					{
-						date: "20220315T09:52:44",
-						content: "Lorem ipsum dolor sit amet",
+						date: "20220317T09:52:44",
+						content: "Ciao Michele",
 						sent: true,
 					},
 					{
-						date: "20220315T09:52:44",
-						content: "Lorem ipsum dolor sit amet",
+						date: "20220317T09:53:50",
+						content: "Ciao Alessandro",
 						sent: false,
 					},
 					{
-						date: "20220315T09:52:44",
-						content: "Lorem ipsum dolor sit amet",
+						date: "20220317T10:00:13",
+						content: "Raccontami una barzelletta",
+						sent: true,
+					},
+					{
+						date: "20220317T10:08:25",
+						content:
+							"Un basso e una chitarra litigano: non riescono a trovare un accordo",
 						sent: false,
 					},
 					{
-						date: "20220315T09:52:44",
-						content: "Lorem ipsum dolor sit amet",
+						date: "20220317T10:09:55",
+						content: "🤦🏻",
 						sent: true,
 					},
 				],
@@ -41,24 +48,24 @@ const app = new Vue({
 				newMessage: "",
 				messages: [
 					{
-						date: "20220315T09:52:44",
-						content: "Lorem ipsum dolor sit amet",
+						date: "20220316T20:12:37",
+						content: "Ciao Alessandro!",
 						sent: false,
 					},
 					{
-						date: "20220315T09:52:44",
-						content: "Lorem ipsum dolor sit amet",
+						date: "20220316T20:13:44",
+						content: "Ciao Fabio, come stai?",
 						sent: true,
 					},
 					{
-						date: "20220315T09:52:44",
-						content: "Lorem ipsum dolor sit amet",
-						sent: true,
-					},
-					{
-						date: "20220315T09:52:44",
-						content: "Lorem ipsum dolor sit amet",
+						date: "20220316T20:14:55",
+						content: "Bene, grazie! E tu?",
 						sent: false,
+					},
+					{
+						date: "20220316T20:16:13",
+						content: "Non c'è male... VueJS mi tormenta!",
+						sent: true,
 					},
 				],
 			},
@@ -69,27 +76,29 @@ const app = new Vue({
 				messages: [
 					{
 						date: "20220315T09:52:44",
-						content: "Lorem ipsum dolor sit amet",
+						content:
+							"Ciao Samuele, non ti dimenticare di portarmi quei documenti!",
 						sent: true,
 					},
 					{
-						date: "20220315T09:52:44",
-						content: "Lorem ipsum dolor sit amet",
+						date: "20220315T09:55:14",
+						content: "Quali documenti?",
 						sent: false,
 					},
 					{
-						date: "20220315T09:52:44",
-						content: "Lorem ipsum dolor sit amet",
+						date: "20220315T09:56:34",
+						content: "Quelli necessari per il contratto",
 						sent: true,
 					},
 					{
-						date: "20220315T09:52:44",
-						content: "Lorem ipsum dolor sit amet",
+						date: "20220315T10:01:27",
+						content:
+							"Ok, capo. Fammi sapere quando posso chiamarti che devo chiederti un paio di informazioni a riguardo.",
 						sent: false,
 					},
 				],
 			},
-			{
+			/*{
 				name: "Alessandro B.",
 				image: "img/avatar_4.jpg",
 				newMessage: "",
@@ -142,7 +151,7 @@ const app = new Vue({
 						sent: false,
 					},
 				],
-			},
+			},*/
 			{
 				name: "Claudia",
 				image: "img/avatar_6.jpg",
@@ -150,27 +159,27 @@ const app = new Vue({
 				messages: [
 					{
 						date: "20220315T09:52:44",
-						content: "Lorem ipsum dolor sit amet",
+						content: "Alessandro come sta andando il corso?",
 						sent: false,
 					},
 					{
-						date: "20220315T09:52:44",
-						content: "Lorem ipsum dolor sit amet",
+						date: "20220315T09:53:14",
+						content: "Bene!",
 						sent: true,
 					},
 					{
-						date: "20220315T09:52:44",
-						content: "Lorem ipsum dolor sit amet",
+						date: "20220315T09:54:25",
+						content: "Sto imparando un sacco di cose nuove",
 						sent: true,
 					},
 					{
-						date: "20220315T09:52:44",
-						content: "Lorem ipsum dolor sit amet",
+						date: "20220315T09:55:57",
+						content: "E la pratica di pomeriggio è molto divertente",
 						sent: true,
 					},
 				],
 			},
-			{
+			/*{
 				name: "Federico",
 				image: "img/avatar_7.jpg",
 				newMessage: "",
@@ -223,7 +232,7 @@ const app = new Vue({
 						sent: true,
 					},
 				],
-			},
+			},*/
 		],
 	},
 	methods: {
@@ -252,7 +261,21 @@ const app = new Vue({
 			setTimeout(() => {
 				this.replyMessage(index);
 			}, 1000);
-		}
+		},
+		switchMenu(index) {
+			this.msgMenuActive = this.msgMenuActive === index ? null : index;
+		},
+		deleteMsg(index) {
+			this.chatArray[this.activeIndex].messages.splice(index, 1);
+			this.switchMenu(index);
+		},
+		trimString(string) {
+			if (string.length > 30) {
+				return string.substring(0, 30) + "...";
+			} else {
+				return string;
+			}
+		},
 	},
 	computed: {
 		filteredContacts() {
